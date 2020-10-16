08d094339cba5e8de54d8686aecd6b01774310ce
Vicente Herrera <vicenteherrera@vicenteherrera.com>
2020-10-16 19:35:34 +0200

    Removed bash scripts that are very specific and not valuable for general usage

--- csv_rules.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# merge_tags
-python3 falco_analyzer.py get_csv_tags falco_rules_merged.yaml falco_rules.csv
-python3 falco_analyzer.py get_csv_tags k8s_audit_rules_merged.yaml k8s_audit_rules.csv
